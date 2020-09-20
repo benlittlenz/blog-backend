246d8ad6699ab4edabf432939a26ce54fc88b026
benlittlenz <ben@pop-os.localdomain>
2020-09-20 21:06:00 +1200

    added recent posts to index controller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -13,18 +13,18 @@ class IndexController extends Controller
     public function index()
     {
         $posts = Post::where('is_published', false)->orderBy('id', 'desc')->get();
-
         $featured_posts = Post::where('is_published',true)->where('is_featured',true)->orderBy('id','desc')->take(5)->get();
-
         $categories = Category::all();
-
         $tags = Tag::all();
+
+        $recent_posts = Post::where('is_published',true)->orderBy('created_at','desc')->take(5)->get();
 
         return view('home', array(
             'posts' => $posts,
             'featured_posts' => $featured_posts,
             'categories' => $categories,
             'tags' => $tags,
+            'recent_posts', $recent_posts,
         ));
     }
 }
