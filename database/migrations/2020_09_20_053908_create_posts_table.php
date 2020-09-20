adb7eb0fc42e348cb1aba37bb87cd6f8edca892d
benlittlenz <ben@pop-os.localdomain>
2020-09-20 17:45:42 +1200

    initial commit and table setup

---
+++ database/migrations/2020_09_20_053908_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('category_id');
+            $table->string('title');
+            $table->string('slug')->unqiue();
+            $table->text('content');
+            $table->string('featured_image')->nullable();
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('is_published')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
